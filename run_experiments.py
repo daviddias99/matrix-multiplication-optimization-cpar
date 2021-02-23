ed3c6c8e14cff3a50a04a3b55e4ce8bc814111db
luispcunha <luispplmcunha@gmail.com>
2021-02-23 16:00:48 +0000

    Change docs dir

--- run_experiments.py
+++ run_experiments.py
@@ -1,13 +1,27 @@
 import os
 import subprocess
 
-cpp_file = os.path.join(os.path.dirname(__file__), 'matrixprod_cpp')
+import pandas as pd
 
-process = subprocess.run(['g++', '-O2', 'matrixprod.cpp', '-o', cpp_file, '-lpapi'])
+os.makedirs('out', exist_ok=True)
 
-process = subprocess.run([cpp_file],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         universal_newlines=True)
+out_path = os.path.join(os.path.dirname(__file__), 'out')
+src_path = os.path.join(os.path.dirname(__file__), 'src')
 
-print(process)
+
+cpp_src = os.path.join(src_path, 'matrixprod.cpp')
+cpp_out = os.path.join(out_path, 'matrixprod_cpp')
+
+process = subprocess.run(['g++', '-O2', cpp_src, '-o', cpp_out, '-lpapi'])
+
+
+process = subprocess.run([cpp_out, '1', '100', '3'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True)
+
+df = pd.DataFrame(
+    [row.split(',') for row in process.stdout.split('\n') if row.strip() != ''],
+    columns=['Time', 'L1 DCM', 'L2 DCM'])
+
+print(df)
