4bc5b568b912b725b78ca2c9a12148f0d3725bbc
luispcunha <luispplmcunha@gmail.com>
2021-02-27 18:31:27 +0000

    Add script to run first two experiments

--- run_experiments.py
+++ run_experiments.py
@@ -1,27 +1,67 @@
+from datetime import datetime
 import os
 import subprocess
-
 import pandas as pd
+from datetime import datetime
 
 os.makedirs('out', exist_ok=True)
+os.makedirs('results', exist_ok=True)
 
-out_path = os.path.join(os.path.dirname(__file__), 'out')
 src_path = os.path.join(os.path.dirname(__file__), 'src')
+out_path = os.path.join(os.path.dirname(__file__), 'out')
+results_path = os.path.join(os.path.dirname(__file__), 'results')
+
+cpp_path = {
+    'src': os.path.join(src_path, 'matrixprod.cpp'),
+    'out': os.path.join(out_path, 'matrixprod_cpp')
+}
+python_path = os.path.join(src_path, 'matrixprod.py')
+
+run_cmd = {
+    'cpp': [cpp_path['out']],
+    'py': ['python', python_path]
+}
 
 
-cpp_src = os.path.join(src_path, 'matrixprod.cpp')
-cpp_out = os.path.join(out_path, 'matrixprod_cpp')
+def compile_cpp():
+    subprocess.run(['g++', '-O2', cpp_path['src'], '-o', cpp_path['out'], '-lpapi'])
 
-process = subprocess.run(['g++', '-O2', cpp_src, '-o', cpp_out, '-lpapi'])
+def run(language, algorithm, size, n_runs):
+    process = subprocess.run(run_cmd[language] + [str(algorithm), str(size), str(n_runs)],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             universal_newlines=True)
 
+    return process.stdout
 
-process = subprocess.run([cpp_out, '1', '100', '15'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True)
+def parse_output(output, other_data):
+    return [other_data + row.split(',') for row in output.split('\n') if row.strip() != '']
 
-df = pd.DataFrame(
-    [row.split(',') for row in process.stdout.split('\n') if row.strip() != ''],
-    columns=['Time', 'L1 DCM', 'L2 DCM'])
+def run_experiments(start, end, step, n_runs, algorithms, languages):
 
-print(df)
+    results = []
+
+    for alg in algorithms:
+        for lang in languages:
+            for size in range(start, end+step, step):
+                output = run(lang, 1, size, n_runs)
+                parsed_output = parse_output(output, [lang, alg, size])
+                results += parsed_output
+
+    return pd.DataFrame(results, columns=['Language', 'Algorithm', 'Matrix Size', 'Time', 'L1 DCM', 'L2 DCM'])
+
+compile_cpp()
+
+n_runs = 3
+
+exp_1_results = run_experiments(600, 3000, 400, n_runs, [1], ['cpp', 'py'])
+exp_1_results.to_csv(os.path.join(results_path, 'exp_1_{}.csv'.format(datetime.now())), index=False, header=True)
+
+exp_2a_results = run_experiments(600, 3000, 400, n_runs, [2], ['cpp', 'py'])
+exp_2a_results.to_csv(os.path.join(results_path, 'exp_2a_{}.csv'.format(
+    datetime.now())), index=False, header=True)
+
+exp_2b_results = run_experiments(4096, 10240, 2048, n_runs, [2], ['cpp'])
+exp_2b_results.to_csv(os.path.join(results_path, 'exp_2b_{}.csv'.format(
+    datetime.now())), index=False, header=True)
+
