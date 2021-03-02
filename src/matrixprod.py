043b8f4a76715583b78117c5778551391ae1ba45
daviddias99 <up201705373@fe.up.pt>
2021-03-02 08:58:33 +0000

    Verified python algorithm

--- src/matrixprod.py
+++ src/matrixprod.py
@@ -1,8 +1,9 @@
 import sys
 import time
+import numpy as np
 
 def mult_matrix_1(mat_size, mat_a, mat_b, _):
-  mat_c = []
+  mat_c = [0]*(mat_size**2)
 
   start = time.process_time()
 
@@ -13,7 +14,7 @@ def mult_matrix_1(mat_size, mat_a, mat_b, _):
       for k in range(mat_size):
         temp += mat_a[i * mat_size + k] * mat_b[k * mat_size + j]
 
-      mat_c += [temp]
+      mat_c[i * mat_size + j] = temp
 
   end = time.process_time()
 
@@ -34,20 +35,20 @@ def mult_matrix_2(mat_size, mat_a, mat_b, _):
   return end - start, mat_c
 
 def mult_matrix_3(mat_size, mat_a, mat_b, block_size):
-  mat_c = []
+  mat_c = [0]*(mat_size**2)
 
   start = time.process_time()
 
   for jj in range(0, mat_size, block_size):
     for kk in range(0, mat_size, block_size):
       for i in range(mat_size):
-        for j in range(jj, min(jj + block_size, mat_size)):
+        for j in range(jj, jj + block_size):
           temp = 0
 
-          for k in range(kk, min(kk + block_size, mat_size)):
+          for k in range(kk, kk + block_size):
             temp += mat_a[i * mat_size + k] * mat_b[k * mat_size + j]
 
-          mat_c += [temp]
+          mat_c[i * mat_size + j] += temp
 
   end = time.process_time()
 
@@ -61,8 +62,8 @@ def mult_matrix_4(mat_size, mat_a, mat_b, block_size):
   for jj in range(0, mat_size, block_size):
     for kk in range(0, mat_size, block_size):
       for i in range(mat_size):
-        for k in range(kk, min(kk + block_size, mat_size)):
-          for j in range(jj, min(jj + block_size, mat_size)):
+        for k in range(kk, kk + block_size):
+          for j in range(jj, jj + block_size):
             mat_c[i*mat_size + j] += mat_a[i * mat_size + k] * mat_b[k * mat_size + j]
 
   end = time.process_time()
@@ -79,15 +80,13 @@ def main(args):
   alg_idx = int(args[3]) - 1
   block_size = int(args[4]) if len(args) > 4 else mat_size
 
-  mat_a = [1.0] * mat_size * mat_size
-  mat_b = [i + 1.0 for i in range(mat_size) for j in range(mat_size)]
+  mat_a = [[1] * mat_size] * mat_size
+  mat_b = [[i + 1 for i in range(mat_size)] for j in range(mat_size)]
 
   for _ in range(n_runs):
     mult_matrix = functions[alg_idx]
     elapsed_time, _ = mult_matrix(mat_size, mat_a, mat_b, block_size)
-
     print(f'{elapsed_time}')
-
 
 if __name__ == '__main__':
   main(sys.argv)
